02a07f756e563128e033fb7eec479ca09c46bf19
squeleton <eumeodio@gmail.com>
2024-04-14 07:26:57 -0400

    tercero

---
+++ stockSimple.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import yfinance as yf
+import streamlit as st
+
+st.write("""
+# Data Science Proyect One: Simple Stock Market
+
+Se muestra el stock closing price and volume de GOOGLE!!!
+""")
+
+tickerSymbol = 'APPL'
+tickerData = yf.Ticker(tickerSymbol)
+tickerDF = tickerData.history(period="id", start ="2018-12-25", end="2023-12-20")
+
+st.line_chart(tickerDF.Close)
+st.line_chart(tickerDF.Volume)
